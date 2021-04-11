ca2686d7980843f78b75b38873c93cc7db1e7c45
vibhudiwan <77778060+vibhudiwan@users.noreply.github.com>
2021-04-11 11:47:34 +0530

    Update Bob.js

--- Bob.js
+++ Bob.js
@@ -1,30 +1,23 @@
 class Bob {
-    constructor(x,y,r) {
-var options ={
-   isStatic:false,
-   friction:0.1,
-   density:0.1,
-  restitution:1,
-}
-   this.x = x;
-   this.y = y;
-   this.r = r; 
-   this.body = Bodies.circle(this.x,this.y,this.r/2 ,options)
-   
-   World.add(world,this.body)
+    constructor(x, y, diameter) {
+      var options = {
+            isStatic: false,
+            restitution: 1.2,
+            friction: 0.4,
+            density: 1.5
+      }
+    this.body = Bodies.circle(x, y, diameter/2, options);
+    this.diameter = diameter;
+      
+    World.add(world, this.body);
     }
-    display() {
-
-    var pos = this.body.position;
-    push()
-    translate(pos.x,pos.y)
-    rotate(this.body.angle);
-    fill("lime");
-    ellipseMode(CENTER)
-    ellipse(0,0,this.r,this.r)
-    pop()
-   
-   
- 
+    display(){
+        var pos = this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rectMode(CENTER);
+        fill("red");
+        ellipse(0, 0, this.diameter);
+        pop();
     }
-}
+  };
